416c771e6bf17e458ff8aa4bed6b5cb0b705c026
Manuel Soler <manusoler@gmail.com>
2018-10-26 21:18:36 +0200

    First commit with initial sctructure

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import a library to help create the component
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+// Create a component
+
+const App = () => (
+  <View>
+    <Header title="Albums" />
+    <AlbumList />
+  </View>
+);
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);

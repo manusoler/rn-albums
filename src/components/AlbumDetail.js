70a9f2aa4e379324d71733a51082c1375e27899a
Manuel Soler <manusoler@gmail.com>
2018-10-28 23:04:32 +0100

    Card and card sections components created

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,5 +1,31 @@
 import React from 'react';
+import { Text, Image } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
-export default function AlbumDetail() {
-  return <div />;
-}
+const AlbumDetail = props => {
+  const { album } = props;
+  return (
+    <Card>
+      <CardSection>
+        <Image
+          style={{ width: 50, height: 50 }}
+          source={{ uri: album.thumbnail_image }}
+        />
+        <Text>{album.title}</Text>
+        <Text>{album.artist}</Text>
+      </CardSection>
+      <CardSection>
+        <Image
+          style={{ width: 50, height: 50 }}
+          source={{ uri: album.image }}
+        />
+      </CardSection>
+      <CardSection>
+        <Text>{album.url}</Text>
+      </CardSection>
+    </Card>
+  );
+};
+
+export default AlbumDetail;
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { albums: [] };
+  }
+
+  componentWillMount() {
+    axios
+      .get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(res => {
+        this.setState({ albums: res.data });
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
-    return (
-      <View>
-        <Text>Album List!!</Text>
-      </View>
-    );
+    const { albums } = this.state;
+    let albumsContent = null;
+    if (albums && albums.length) {
+      albumsContent = albums.map(album => (
+        <AlbumDetail key={album.title} album={album} />
+      ));
+    }
+    return <View>{albumsContent}</View>;
   }
 }
 

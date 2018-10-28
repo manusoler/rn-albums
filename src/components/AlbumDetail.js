36b5aec89c4c78718e4af14bf79ee6a6ca11318f
Manuel Soler <manusoler@gmail.com>
2018-10-28 23:43:46 +0100

    Expanded AlbumDetail component to add styles

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,31 +1,55 @@
 import React from 'react';
-import { Text, Image } from 'react-native';
+import { Text, Image, View } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = props => {
-  const { album } = props;
-  return (
-    <Card>
-      <CardSection>
+const AlbumDetail = ({ album }) => (
+  <Card>
+    <CardSection>
+      <View style={styles.thumbnailContainerStyle}>
         <Image
-          style={{ width: 50, height: 50 }}
+          style={styles.headerImageStyles}
           source={{ uri: album.thumbnail_image }}
         />
-        <Text>{album.title}</Text>
+      </View>
+      <View style={styles.headerContentStyles}>
+        <Text style={styles.headerTextStyle}>{album.title}</Text>
         <Text>{album.artist}</Text>
-      </CardSection>
-      <CardSection>
-        <Image
-          style={{ width: 50, height: 50 }}
-          source={{ uri: album.image }}
-        />
-      </CardSection>
-      <CardSection>
-        <Text>{album.url}</Text>
-      </CardSection>
-    </Card>
-  );
+      </View>
+    </CardSection>
+    <CardSection>
+      <Image style={styles.albumImageStyles} source={{ uri: album.image }} />
+    </CardSection>
+    <CardSection>
+      <Text>{album.url}</Text>
+    </CardSection>
+  </Card>
+);
+
+const styles = {
+  headerContentStyles: {
+    marginLeft: 5,
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  headerImageStyles: {
+    width: 50,
+    height: 50
+  },
+  albumImageStyles: {
+    height: 300,
+    flex: 1,
+    width: null
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  }
 };
 
 export default AlbumDetail;
